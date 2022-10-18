3066accb6896df6dfc72ad2be15fb26215106c9d
chrispaton01 <christian.paton01@gmail.com>
2022-10-18 18:36:48 +0100

    progress push 7

--- src/components/Contact/Footer.js
+++ src/components/Contact/Footer.js
@@ -3,7 +3,17 @@ import './Footer.css';
 const Footer = () => {
   return (
     <div>
-      <div className='Footer'>Contact: example@gmail.com</div>
+      <div className='Footer'>
+        <p>email: example@gmail.com</p>
+        <a
+          className='Creator-tag'
+          target='_blank'
+          href='https://www.linkedin.com/in/christian-samuel-paton/'
+          rel='noopener noreferrer'
+        >
+          <p>Designed & Built by Christian P. ©2022</p>
+        </a>
+      </div>
     </div>
   );
 };
