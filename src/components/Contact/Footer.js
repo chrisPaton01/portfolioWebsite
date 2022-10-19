bdace3d74d8f0562eeb17c48ccaed44b1e4ad7ef
chrispaton01 <christian.paton01@gmail.com>
2022-10-19 13:12:55 +0100

    shashank error help

--- src/components/Contact/Footer.js
+++ src/components/Contact/Footer.js
@@ -4,9 +4,11 @@ const Footer = () => {
   return (
     <div>
       <div className='Footer'>
-        <p>email: example@gmail.com</p>
+        <p className='Creator-tag1'>
+          email: <a href='mailto:someone@example.com'>example@gmail.com</a>
+        </p>
         <a
-          className='Creator-tag'
+          className='Creator-tag2'
           target='_blank'
           href='https://www.linkedin.com/in/christian-samuel-paton/'
           rel='noopener noreferrer'
