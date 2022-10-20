f3106668448ea8fe356a635549146ff9ec988710
chrispaton01 <christian.paton01@gmail.com>
2022-10-20 22:24:00 +0100

    window scaling fixes and css animations

--- src/components/Activities/Hobbies.js
+++ src/components/Activities/Hobbies.js
@@ -7,17 +7,17 @@ const Hobbies = () => {
       <br></br>
       <br></br>
       <iframe
-        title='Berserk Dragon Slayer Fan Art'
+        title='Kirby Fan Art'
         scrolling='0'
-        src='https://sketchfab.com/3d-models/dragon-slayer-berserk-fan-art-b21cc2759ab145a8afc1659440371c4e/embed?ui_infos=0&amp;ui_watermark=0&amp;ui_help=0&amp;ui_settings=0&amp;ui_inspector=0&amp;ui_annotations=0&amp;ui_stop=0&amp;ui_vr=0&amp;preload=1&amp;autostart=1&amp;ui_hint=2&amp;autospin=0.2'
+        src='https://sketchfab.com/3d-models/kirby-fan-art-5613d284c2494c818adf87851411e999/embed?ui_infos=0&amp;ui_watermark=0&amp;ui_help=0&amp;ui_settings=0&amp;ui_inspector=0&amp;ui_annotations=0&amp;ui_stop=0&amp;ui_vr=0&amp;preload=1&amp;autostart=1&amp;ui_hint=2&amp;autospin=0.2'
         width='100%'
         height='500px'
       ></iframe>
       &nbsp;
       <iframe
-        title='Kirby Fan Art'
+        title='Berserk Dragon Slayer Fan Art'
         scrolling='0'
-        src='https://sketchfab.com/3d-models/kirby-fan-art-5613d284c2494c818adf87851411e999/embed?ui_infos=0&amp;ui_watermark=0&amp;ui_help=0&amp;ui_settings=0&amp;ui_inspector=0&amp;ui_annotations=0&amp;ui_stop=0&amp;ui_vr=0&amp;preload=1&amp;autostart=1&amp;ui_hint=2&amp;autospin=0.2'
+        src='https://sketchfab.com/3d-models/dragon-slayer-berserk-fan-art-b21cc2759ab145a8afc1659440371c4e/embed?ui_infos=0&amp;ui_watermark=0&amp;ui_help=0&amp;ui_settings=0&amp;ui_inspector=0&amp;ui_annotations=0&amp;ui_stop=0&amp;ui_vr=0&amp;preload=1&amp;autostart=1&amp;ui_hint=2&amp;autospin=0.2'
         width='100%'
         height='500px'
       ></iframe>
